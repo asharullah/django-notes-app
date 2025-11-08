2e1e0f12266a96f060981bc598af46d38f76837b
asharullah <90705818+asharullah@users.noreply.github.com>
2025-11-08 13:12:00 +0500

    Set default database configuration values

--- notesapp/settings.py
+++ notesapp/settings.py
@@ -88,16 +88,16 @@ WSGI_APPLICATION = 'notesapp.wsgi.application'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': os.getenv("DB_NAME"),
-        'USER': os.getenv("DB_USER"),
-        'PASSWORD': os.getenv("DB_PASSWORD"),
-        'HOST': os.getenv("DB_HOST"),
-        'PORT': os.getenv("DB_PORT"),
+        'NAME': os.getenv("DB_NAME", "notes_db"),
+        'USER': os.getenv("DB_USER", "notes_user"),
+        'PASSWORD': os.getenv("DB_PASSWORD", "password"),
+        'HOST': os.getenv("DB_HOST", "notes-mysql"),
+        'PORT': os.getenv("DB_PORT", "3306"),
     }
 }
+
 
 
 # Password validation
